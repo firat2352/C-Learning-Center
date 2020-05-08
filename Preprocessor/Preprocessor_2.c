25b79ef4bd10897c106881acae64441a2e8ccd40
firat2352 <firatcelik52@gmail.com>
2020-05-08 18:45:52 +0300

    trick2

---
+++ Preprocessor/Preprocessor_2.c
@@ -0,0 +1,12 @@
+//Trick 2 with token-pasting operator
+
+#include <stdio.h>
+#define mainfunction  m##a##i##n
+
+int mainfunction()
+{
+
+	printf("C PROGRAM WITHOUT USING MAIN FUNCTION ");
+
+return 0;
+}
